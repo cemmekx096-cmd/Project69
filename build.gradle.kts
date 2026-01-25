c6cd515507fb92ebad7878831ac55192a8f3075e
cemmekx096-cmd <cemekx096@gmail.com>
2026-01-26 02:13:00 +0700

    Initial Anichin extension5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,16 @@
-// Top-level build file
-plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("com.android.library") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.jvm") version "2.1.0" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "12.1.2" apply false
-}
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget as KotlinJvmTarget
 
 allprojects {
     repositories {
-        google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+        google()
+        maven(url = "https://jitpack.io")
     }
-}
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+    tasks.withType<KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(KotlinJvmTarget.JVM_1_8)
+        }
+    }
 }
---
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -0,0 +1,5 @@
+object AndroidConfig {
+    const val compileSdk = 36
+    const val minSdk = 24
+    const val targetSdk = 36
+}
