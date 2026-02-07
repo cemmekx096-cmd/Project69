6eb66aff2a55de397d2e350e04836ea51fbbeae5
cemmekx096-cmd <cemekx096@gmail.com>
2026-02-07 17:44:43 +0700

    update lk21movies

--- src/id/lk21movies/src/eu/kanade/tachiyomi/animeextension/id/Lk21Movies/LK21Filters.kt
+++ src/id/lk21movies/src/eu/kanade/tachiyomi/animeextension/id/Lk21Movies/LK21Filters.kt
@@ -94,4 +94,34 @@ object LK21Filters {
     ) : AnimeFilter.Select<String>(displayName, vals.map { it.first }.toTypedArray()) {
         fun toUriPart() = vals[state].second
     }
+    fun getFilterList(): AnimeFilterList = FILTER_LIST
+
+    // Data class untuk search parameters
+    data class SearchParameters(
+        val genre: String = "",
+        val country: String = "",
+    )
+
+    // Extract search parameters dari filters
+    fun getSearchParameters(filters: AnimeFilterList): SearchParameters {
+        var genre = ""
+        var country = ""
+
+        filters.forEach { filter ->
+            when (filter) {
+                is GenreFilter -> {
+                    if (filter.state != 0) {
+                        genre = filter.toUriPart()
+                    }
+                }
+                is CountryFilter -> {
+                    if (filter.state != 0) {
+                        country = filter.toUriPart()
+                    }
+                }
+            }
+        }
+
+        return SearchParameters(genre, country)
+    }
 }
