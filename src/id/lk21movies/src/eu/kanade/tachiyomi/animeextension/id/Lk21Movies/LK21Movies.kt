8ba2723c63b06e8989225a81866718727cd22e4b
cemmekx096-cmd <cemekx096@gmail.com>
2026-02-07 20:53:00 +0700

    update lk21movies

--- src/id/lk21movies/src/eu/kanade/tachiyomi/animeextension/id/Lk21Movies/LK21Movies.kt
+++ src/id/lk21movies/src/eu/kanade/tachiyomi/animeextension/id/Lk21Movies/LK21Movies.kt
@@ -103,7 +103,6 @@ class LK21Movies : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
             add("User-Agent", userAgent)
             add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
             add("Accept-Language", "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7")
-            add("Referer", baseUrl)
         }
     }
 
